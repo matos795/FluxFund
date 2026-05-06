a5abc67afbab07bc54bcb9557deb26617c868f42
Seu Nome <seuemail@email.com>
2026-05-06 10:50:03 -0300

    create Spring Boot project

---
+++ src/test/java/com/fluxfund/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluxfund.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
